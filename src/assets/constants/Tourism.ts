8f6a23065bfae2915b832ae21786efde15b6df76
Mussaddiq <mussaddiqmahmood7@gmail.com>
2024-02-25 17:29:18 +0500

    footer and tourism

---
+++ src/assets/constants/Tourism.ts
@@ -0,0 +1,13 @@
+import Makha from "../home/herosection/makha.jpg"
+import Madina from "../home/herosection/madina.jpg"
+import Minar from "../home/herosection/minar.jpg"
+import FasalMasjid from "../home/herosection/fasalmasjid.jpg"
+import Inside from "../home/herosection/inside.jpg"
+import Saven from "../home/herosection/saven.jpg"
+
+export const tourism = [
+    {image:Makha, category:"Makha", description:"dfdfdfd", Link:"one"},
+    {image:Makha, category:"Makha", description:"dfdfdfd", Link:"one"},
+    {image:Makha, category:"Makha", description:"dfdfdfd", Link:"one"},
+    {image:Makha, category:"Makha", description:"dfdfdfd", Link:"one"}
+]
